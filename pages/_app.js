b0a3a27c04df19a87cd173103ddb2f431ef84655
Jared Grippe <jared@deadlyicon.com>
2022-06-22 17:23:57 -0700

    added MUI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+import React from 'react'
+import { CacheProvider } from '@emotion/react'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+
+import createEmotionCache from '../lib/createEmotionCache'
+import darkTheme from '../styles/themes/darkTheme'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+
+const MyApp = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to YOUR FACE!
+          Welcome to YOUR FACE!!!
         </h1>
 
         <p className={styles.description}>
